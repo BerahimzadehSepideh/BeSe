08b02a6655d2499011e57c77365464b391973fa3
Shen Tong <77437583+surgecn@users.noreply.github.com>
2021-01-26 18:57:57 +0100

    Update LoginActivity.java

--- app/src/main/java/com/example/gesco/MedicationActivity.java
+++ app/src/main/java/com/example/gesco/MedicationActivity.java
@@ -2,6 +2,7 @@ package com.example.gesco;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +14,15 @@ public class MedicationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medication);
 
+        Button button_patient = (Button) findViewById(R.id.buttonPatient);
+        button_patient.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MedicationActivity.this, MyMedicationPatientActivity.class);
+                startActivity(intent);
+            }
+            });
 
         Button button_back = (Button) findViewById(R.id.button_back2);
         button_back.setOnClickListener(new View.OnClickListener() {
